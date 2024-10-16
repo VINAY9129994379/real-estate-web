e1b993d36629911c62bb0f44e3dd202e35440897
VINAY9129994379 <vinay2272001@gmail.com>
2024-10-16 23:09:10 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+import { Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import SignUp from './pages/SignUp';
+import Properties from './pages/Properties';
+import News from './pages/News';
+import Team from './pages/Team';
+import Footer from './components/Footer';
+import ContactMenu from './pages/ContactMenu';
+import AllProperty from './components/AllProperty';
+import BuyProperty from './components/BuyProperty';
+import RentProperties from './components/RentProperties';
+import './App.css'
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <Header/>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route className='property-nav'>
+           <Route path='/properties' element={<Properties/>}/>
+            <Route className='property-dropdown'>
+              <Route className='property-dropdown-section'>
+                <Route path='/all' element={<AllProperty/>}/>
+                <Route path='/buy' element={<BuyProperty/>}/>
+                <Route path='/rent' element={<RentProperties/>}/>
+
+              </Route>
+            </Route>
+          </Route>
+        
+        <Route path='/news' element={<News />} />
+        <Route path='/team' element={<Team />} />
+        <Route path='/contactmenu' element={<ContactMenu />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+      </Routes>
+      <Footer/>
     </div>
   );
 }
